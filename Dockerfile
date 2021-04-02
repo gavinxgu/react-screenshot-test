046702bf29145c5eac67cfb159e575788d959fed
guxiang <guxiang@bytedance.com>
2021-04-02 13:27:48 +0800

    chore: update pkg version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mcr.microsoft.com/playwright
+FROM mcr.microsoft.com/playwright:bionic
 RUN apt-get -qqy update && \
   apt-get -qqy --no-install-recommends install \
   fonts-roboto \
--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
 VERSION=$(node -e "console.log(require(\"./package.json\").version)")
+docker pull mcr.microsoft.com/playwright:bionic
 IMAGE_TAG=gavinxgu/react-screenshot-test:$VERSION
 docker build -t $IMAGE_TAG .
 docker push $IMAGE_TAG
